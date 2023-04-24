fd9b00e8b4cab2ab6d21e764af95cf509368e589
Roberto Liccardo <roberto_liccardo@outlook.it>
2023-04-24 11:36:47 +0200

    Added both LinearSearch and BinarySearch algorithms implementations

---
+++ src/Searching/BinarySearch.java
@@ -0,0 +1,58 @@
+package Searching;
+
+import java.util.Arrays;
+
+/**
+ * The {@code BinarySearch} class provides a static method for binary
+ * searching for an integer in a sorted array of integers.
+ * <p>
+ * The <em>indexOf</em> operation takes logarithmic time in the worst case.
+ * <p>
+ */
+public class BinarySearch {
+
+    /**
+     * This class should not be instantiated.
+     */
+    private BinarySearch() { }
+
+    /**
+     * Returns the index of the specified key in the specified array.
+     *
+     * @param a   the array of integers, must be sorted in ascending order
+     * @param key the search key
+     * @return index of the key in array {@code a} if present; {@code -1} otherwise
+     */
+    public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;   // lower the hi below the mid-point
+            else if (key > a[mid]) lo = mid + 1;   // raise the lo above the mid-point
+            else return mid;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Unit test the {@code LinkedStack} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        int key = 17;
+
+        // sort the array
+        Arrays.sort(a);
+
+        // perform the binary search
+        int result = BinarySearch.indexOf(a, key);
+        if (result == -1) System.out.println("Couldn't find the key");
+        else System.out.println("The key " + key + " is in the array at index: " + result);
+    }
+
+}
---
+++ src/Searching/LinearSearch.java
@@ -0,0 +1,54 @@
+package Searching;
+
+import java.util.Arrays;
+
+/**
+ * The {@code LinearSearch} class provides a static method for binary
+ * searching for an integer in a sorted array of integers.
+ * <p>
+ * The <em>indexOf</em> operations takes linear time in the worst case.
+ * <p>
+ */
+public class LinearSearch {
+
+    /**
+     * This class should not be instantiated.
+     */
+    private LinearSearch() { }
+
+    /**
+     * Returns the index of the specified key in the specified array.
+     *
+     * @param a   the array of integers, must be sorted in ascending order
+     * @param key the search key
+     * @return index of the key in array {@code a} if present; {@code -1} otherwise
+     */
+    public static int indexOf(int[] a, int key) {
+        int N = a.length;
+
+        for (int i = 0; i < N; i++) {
+            if (a[i] == key) return i;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Unit test the {@code LinkedStack} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        int key = 17;
+
+        // sort the array
+        Arrays.sort(a);
+
+        // perform the binary search
+        int result = LinearSearch.indexOf(a, key);
+        if (result == -1) System.out.println("Couldn't find the key");
+        else System.out.println("The key " + key + " is in the array at index: " + result);
+    }
+
+}
