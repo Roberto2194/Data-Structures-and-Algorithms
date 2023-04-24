a1a128f4bd9d6118860ec860304da73b3e841bda
Roberto Liccardo <roberto_liccardo@outlook.it>
2023-04-24 12:32:42 +0200

    Added generic implementation of SelectionSort using Comparable interface

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -1,2 +1,53 @@
-package Sorting;public class SelectionSort {
+package Sorting;
+
+public class SelectionSort {
+
+    /**
+     * This class should not be instantiated.
+     */
+    private SelectionSort() { }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     *
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) min = j;
+            }
+            exch(a, i, min);
+        }
+    }
+
+    // is the first element v less than the second w?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    /**
+     * Unit test the {@code LinkedStack} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        Integer[] a = {2, 23, 12, 17, 11, 9, 77, 45, 62, 22};
+
+        SelectionSort.sort(a);
+
+        for (int s : a) {
+            System.out.println(s);
+        }
+    }
+
 }
