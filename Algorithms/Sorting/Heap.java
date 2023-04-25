deec07eadc8a2ba6b5fd42b4cfb19f2d775f1c68
Roberto Liccardo <57627290+Roberto2194@users.noreply.github.com>
2023-04-25 19:29:09 +0200

    Added Heapsort algorithm implementation

---
+++ Algorithms/Sorting/Heap.java
@@ -0,0 +1,78 @@
+package Algorithms.Sorting;
+
+/**
+ * The {@code Heap} class provides a static method to sort an array
+ * using <em>heapsort</em>.
+ * <p>
+ * This implementation takes &Theta;(<em>n</em> log <em>n</em>) time
+ * to sort any array of length <em>n</em> (assuming comparisons
+ * take constant time). It makes at most
+ * 2 <em>n</em> log<sub>2</sub> <em>n</em> compares.
+ * <p>
+ * This sorting algorithm is not stable.
+ * It uses &Theta;(1) extra memory (not including the input array).
+ * <p>
+ * <b>Best case</b>: &Omega;(<em>n</em> <em>log</em>(<em>n</em>))<br>
+ * <b>Average case</b>: &Theta;(<em>n</em> <em>log</em>(<em>n</em>))<br>
+ * <b>Worst case</b>: &Omicron;(<em>n</em> <em>log</em>(<em>n</em>))
+ */
+public class Heap {
+
+    // this class should not be instantiated
+    private Heap() { }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     *
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] pq) {
+        int n = pq.length;
+
+        // heapify phase
+        for (int k = n / 2; k >= 1; k--)
+            sink(pq, k, n);
+
+        // sortdown phase
+        int k = n;
+        while (k > 1) {
+            exch(pq, 1, k--);
+            sink(pq, 1, k);
+        }
+    }
+
+    private static void sink(Comparable[] pq, int k, int n) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(pq, j, j + 1)) j++;
+            if (!less(pq, k, j)) break;
+            swap(pq, k, j);
+            k = j;
+        }
+    }
+
+    // print the array, on a single line
+    public static void show(Object[] a) {
+        for (Object key : a) System.out.print(key + " ");
+        System.out.println();
+    }
+
+    // test whether the array entries are in order
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) if (less(a[i], a[i - 1])) return false;
+        return true;
+    }
+
+    // is the first element v less than the second w?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void swap(Object[] a, int i, int j) {
+        Object temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+}
